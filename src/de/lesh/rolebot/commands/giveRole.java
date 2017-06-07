37c0a587fef977d0c8b66b86b75dc15a1e045dc4
Sanduhr32 <Maurice.Craft@t-online.de>
2017-06-07 19:43:43 +0200

    Update giveRole.java

--- src/de/lesh/rolebot/commands/giveRole.java
+++ src/de/lesh/rolebot/commands/giveRole.java
@@ -1,9 +1,7 @@
 package de.lesh.rolebot.commands;
 
 import de.lesh.rolebot.user.bannedList;
-import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.Role;
-import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
@@ -22,11 +20,11 @@ public class giveRole extends ListenerAdapter{
 		String role = e.getMessage().getRawContent().split("\\s+", 2)[1];
 		
 		switch(role){
-			case "beginner" :  e.getGuild().getController().addRolesToMember(e.getMember(), beginnerID).queue(); //.getRoleById(316125948544155649L).;
+			case "beginner" :  { e.getGuild().getController().addRolesToMember(e.getMember(), beginnerID).queue(); break;} //.getRoleById(316125948544155649L).;
 			case "medium" : 
 			case "profi" : 
 				
-			default : e.getChannel().sendMessage("Die angegebene Gruppe ist nicht vorhanden. \n Bitte wähle zwischen beginner , medium oder profi").queue();
+			default : { e.getChannel().sendMessage("Die angegebene Gruppe ist nicht vorhanden. \n Bitte wähle zwischen beginner , medium oder profi").queue(); break; }
 		}
 	}
 }
